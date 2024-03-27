446f0ddfac822b32412e8c1da9342b8b722d9820
Shauna MacFarlane-Okongo <102887963+DrSLMac@users.noreply.github.com>
2024-03-27 12:49:05 -0500

    new functions

--- js-fun-at-the-library/src/book.js
+++ js-fun-at-the-library/src/book.js
@@ -1,12 +1,34 @@
-function createTitle() {
-  
+function createTitle(title) {
+  return `The ${title}`
+}
+
+function buildMainCharacter(name, age, pronouns) {
+  const character = {
+    name: name, 
+    age: age,
+    pronouns: pronouns
+  }
+  return character
+}
+
+function saveReview(review, reviews) {
+  if (!reviews.includes(review)) {
+    reviews.push(review)
+  }
+  return reviews
+}
+
+function calculatePageCount(title) {
+  console.log({title})
+  const letters = title.split('')
+  return letters.length * 20
 }
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
+  buildMainCharacter,
+  saveReview,
+  calculatePageCount,
   // writeBook,
   // editBook
 }
